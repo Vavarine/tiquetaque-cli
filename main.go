e4126185837b44ef4daaf8b1582a06f2693f9ab9
vavarine <evailson.m.barbosa@gmail.com>
2025-09-17 11:24:12 -0300

    feat: checks for cli updates

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/vavarine/ttq/cmd"
 )
 
 var version string = "dev"
 
+type Release struct {
+	TagName string `json:"tag_name"`
+}
+
+func checkForUpdateAsync() {
+	go func() {
+		// evita que o programa trave se a API for lenta
+		client := http.Client{Timeout: 2 * time.Second}
+		resp, err := client.Get("https://api.github.com/repos/Vavarine/tiquetaque-cli/releases/latest")
+		if err != nil {
+			return
+		}
+		defer resp.Body.Close()
+
+		var r Release
+		if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+			return
+		}
+
+		if r.TagName != version {
+			fmt.Fprintf(os.Stderr, "\n⚠️ Nova versão da cli disponível: %s (atual: %s)\n", r.TagName, version)
+		}
+	}()
+}
+
 func main() {
-	fmt.Printf("ttq version: %s\n", version)
+	checkForUpdateAsync()
 
 	cmd.Execute()
 }
